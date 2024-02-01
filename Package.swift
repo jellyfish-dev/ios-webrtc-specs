9217153c1aef08f78ca4a5ea03a0ffb235b1ea32
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2024-02-01 14:18:21 +0100

    Change target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "WebRTC",
-            targets: ["WebRTC"]),
+            targets: ["WebRTB"]),
     ],
     dependencies: [ ],
     targets: [
