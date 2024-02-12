2d96c5214ea044601bcd4590de20fd2e51e09f0a
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2024-02-12 11:41:14 +0100

    Fix

--- Package.swift
+++ Package.swift
@@ -2,17 +2,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "WebRTC",
+    name: "WebRTCMembraneFramework",
     platforms: [.iOS(.v12), .macOS(.v10_13)],
     products: [
         .library(
             name: "WebRTCMembraneFramework",
-            targets: ["WebRTCMembraneFramework"]),
+            targets: ["WebRTC"]),
     ],
     dependencies: [ ],
     targets: [
         .binaryTarget(
-            name: "WebRTC",
+            name: "WebRTCMembraneFramework",
             url: "https://github.com/incubo4u/Specs/releases/download/104.5112.15/WebRTCMembraneFramework.xcframework.zip",
             checksum: "3f050c46c783f60425fde6071a92f0f328786cbf9e44e1282b786e8e5d1fdeff"
         ),
